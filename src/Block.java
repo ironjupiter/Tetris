f17f44a866239d2067f518e31ef22541de547c15
ironjupiter <60822645+ironjupiter@users.noreply.github.com>
2020-02-08 15:38:34 -0800

    moved code 0.1
    I moved the main loop code to a method.

--- src/Block.java
+++ src/Block.java
@@ -21,7 +21,7 @@ public class Block {
 	public boolean willBlockRightSideCollideWith(ArrayList<Block> allStaticBlocks) {
 		int i = 0;
 		while(allStaticBlocks.size() > i) {
-			System.out.println("check" + (i+1));
+//			System.out.println("check" + (i+1));
 			if (this.x + this.width < allStaticBlocks.get(i).x + allStaticBlocks.get(i).width &&
 			this.x + (this.width*2) > allStaticBlocks.get(i).x &&
 			this.y < allStaticBlocks.get(i).y + allStaticBlocks.get(i).height &&
@@ -37,12 +37,12 @@ public class Block {
 	public boolean willBlockBottomSideCollideWith(ArrayList<Block> allStaticBlocks) {
 		int i = 0;
 		while(allStaticBlocks.size() > i) {
-			System.out.println("check" + (i+1));
+//			System.out.println("check" + (i+1));
 			if (this.x < allStaticBlocks.get(i).x + allStaticBlocks.get(i).width &&
 			this.x + this.width > allStaticBlocks.get(i).x &&
 			this.y + this.height< allStaticBlocks.get(i).y + allStaticBlocks.get(i).height &&
 			this.y + (this.height*2)> allStaticBlocks.get(i).y) {
-				System.out.println(true);
+//				System.out.println(true);
 				return true;
 			}
 			i++;
@@ -53,7 +53,7 @@ public class Block {
 	public boolean willBlockLeftSideCollideWith(ArrayList<Block> allStaticBlocks) {
 		int i = 0;
 		while(allStaticBlocks.size() > i) {
-			System.out.println("check" + (i+1));
+//			System.out.println("check" + (i+1));
 			if (this.x - this.width< allStaticBlocks.get(i).x + allStaticBlocks.get(i).width &&
 			this.x > allStaticBlocks.get(i).x &&
 			this.y < allStaticBlocks.get(i).y + allStaticBlocks.get(i).height &&
@@ -79,7 +79,7 @@ public class Block {
 	}
 	
 	public int getY() {
-		System.out.println("blocks current y = " + y);
+//		System.out.println("blocks current y = " + y);
 		return y;
 	}
 	
@@ -103,7 +103,7 @@ public class Block {
 	}
 	
 	public boolean checkIfBlockHitLeftBarrier(Border b) {
-		System.out.println(getTopLeftCorner() + "," + b.x);
+//		System.out.println(getTopLeftCorner() + "," + b.x);
 		if(getTopLeftCorner() <= b.x ) {
 			System.out.println("hit left wall");
 			return true;
--- src/MainGameLoop.java
+++ src/MainGameLoop.java
@@ -2,24 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainGameLoop {
-	static MovablePiece []tetrominos_list = new MovablePiece[7];
-	static int timer = 0;
+	MovablePiece []tetrominos_list = new MovablePiece[7];
+	int timer = 0;
+	GameWindow game_window;
+	PlayableTetrisArea playable_tetris_area;
+	Border bottom_border;
+	Border left_border;
+	Border top_border;
+	Border right_border;
 	public static void main(String[] args) {
-		//set up playing area DONT EDIT THIS PART IF YOU DONT UNDERSTAND THE ALL THE SOURCE CODE
-		GameWindow game_window = new GameWindow();
-		PlayableTetrisArea playable_tetris_area = new PlayableTetrisArea(game_window);
-		Border bottom_border = new Border(100, 560, 200, 10);//Border(x, y, width, height);
-		Border left_border = new Border(bottom_border.x, bottom_border.y-400, 10, 400);
-		Border top_border = new Border(bottom_border.x, left_border.y, bottom_border.width, bottom_border.height);//this one just marks the top
-		Border right_border = new Border((bottom_border.x+bottom_border.width)-10, left_border.y, left_border.width, left_border.height);
+		MainGameLoop mgl = new MainGameLoop();
+		mgl.mainGameLoop();
+	}
+	
+	public void mainGameLoop() {
+	//set up playing area DONT EDIT THIS PART IF YOU DONT UNDERSTAND THE ALL THE SOURCE CODE
+		game_window = new GameWindow();
+		playable_tetris_area = new PlayableTetrisArea(game_window);
+		bottom_border = new Border(100, 560, 200, 10);//Border(x, y, width, height);
+		left_border = new Border(bottom_border.x, bottom_border.y-400, 10, 400);
+		top_border = new Border(bottom_border.x, left_border.y, bottom_border.width, bottom_border.height);//this one just marks the top
+		right_border = new Border((bottom_border.x+bottom_border.width)-10, left_border.y, left_border.width, left_border.height);
 		//create array
 		createTetrominosList(playable_tetris_area, bottom_border, right_border, left_border);
-		
-		//add extra scatered blocks to arraylist
-//		playable_tetris_area.block_list.add(new Block(250, 400, 10, 10, Color.RED));
-//		playable_tetris_area.block_list.add(new Block(110, 500, 10, 10, Color.YELLOW));
-//		playable_tetris_area.block_list.add(new Block(300, 600, 10, 10, Color.BLUE));
-		
+
 		//add panel
 		game_window.addPanel(playable_tetris_area);
 		
@@ -40,11 +46,11 @@ public class MainGameLoop {
 			//if tetromino hit the ground replace it with static blocks and create a new one for the player
 			if(tetromino.checkIfAnyBlockBottomCollided() == true || tetromino.checkIfOneBlockBottomSideWillCollide() == true) {
 				if(timer == 1) {
-						for(int i = 0; i < tetromino.blocks.length;) {
-							playable_tetris_area.block_list.add(new Block(tetromino.blocks[i].x, tetromino.blocks[i].y, tetromino.blocks[i].width, tetromino.blocks[i].height, tetromino.blocks[i].c));
-							System.out.println("static block length: " + playable_tetris_area.block_list.size()); 
-							i++;
-						}
+					for(int i = 0; i < tetromino.blocks.length;) {
+						playable_tetris_area.block_list.add(new Block(tetromino.blocks[i].x, tetromino.blocks[i].y, tetromino.blocks[i].width, tetromino.blocks[i].height, tetromino.blocks[i].c));
+//						System.out.println("static block length: " + playable_tetris_area.block_list.size()); 
+						i++;
+					}
 					tetromino.y = 180;
 					tetromino.x = 180;
 					playable_tetris_area.removeKeyListener(tetromino);
@@ -52,14 +58,15 @@ public class MainGameLoop {
 					playable_tetris_area.addPiece(tetromino);
 					playable_tetris_area.addKeyListener(tetromino);
 					timer = 0;
-					}
+				}
+				
 				timer++;
 			}
 			playable_tetris_area.repaint();
 		}
 	}
 	
-	public static void createTetrominosList(PlayableTetrisArea playable_tetris_area,Border bottom_border,Border right_border,Border left_border) {
+	public void createTetrominosList(PlayableTetrisArea playable_tetris_area,Border bottom_border,Border right_border,Border left_border) {
 		tetrominos_list[0] = new SPiece(playable_tetris_area, bottom_border, right_border, left_border);
 		tetrominos_list[1] = new SquarePiece(playable_tetris_area, bottom_border, right_border, left_border);
 		tetrominos_list[2] = new ZPiece(playable_tetris_area, bottom_border, right_border, left_border);
@@ -69,7 +76,7 @@ public class MainGameLoop {
 		tetrominos_list[6] = new TPiece(playable_tetris_area, bottom_border, right_border, left_border);
 	}
 	
-	public static MovablePiece pickATetromino() {
+	public MovablePiece pickATetromino() {
 		int rand = (int) (Math.random() * tetrominos_list.length);
 		MovablePiece mp = tetrominos_list[rand];
 		return mp;
